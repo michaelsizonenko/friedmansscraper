30dd6776f3c05ffaea0f3ac84eb8489c6beddd0f
michaelsizonenko <dethline@te.net.ua>
2019-10-24 20:23:39 +0300

    override to use microservices

--- friedmansscraper/spiders/friedmans_spider.py
+++ friedmansscraper/spiders/friedmans_spider.py
@@ -1,41 +1,47 @@
 import scrapy
-import csv
+import json
 import validators
+import base64
+from tempfile import NamedTemporaryFile
+
+ALL_FOUND_TWITTERS = "all found twitters"
 
 
 class FriedmansSpider(scrapy.Spider):
     name = "twitter"
-    filename = None
+    data = None
+    header = []
+    result_file = "result.csv"
+    result_index = 0
+    current_row = ""
+    all_twitter_links = []
 
-    def __init__(self, filename=None, **kwargs):
+    def __init__(self, data=None, **kwargs):
         super(FriedmansSpider, self).__init__(**kwargs)
-        if not filename:
-            raise ValueError("%s must have a filename" % type(self).__name__)
-        self.filename = filename
+        if not data:
+            raise ValueError("%s must have a data" % type(self).__name__)
+        self.data = json.loads(base64.b64decode(data))
 
     def parse(self, response):
-        meta = response.meta
-        links = response.xpath('//a/@href').extract()
-        for link in links:
-            if "twitter" in link:
-                print("!!  twitter link found {}   !!".format(link))
+        print("!!!!! parse !!!!")
+        self.all_twitter_links += filter(lambda x: "twitter" in x, response.xpath('//a/@href').extract())
+        print(self.all_twitter_links)
 
     def start_requests(self):
-        name_index = 0
-        email_index = 1
-        urls_index = 2
-
-        with open(self.filename, 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            next(reader)
-            counter = 0
-            for row in reader:
-                counter += 1
-                meta = {
-                    "row_index": counter,
-                    "name": row[name_index],
-                    "email": row[email_index]
-                }
-                urls = filter(validators.url, row[urls_index:])
-                for url in urls:
-                    yield scrapy.Request(url=url, callback=self.parse, meta=meta)
+        try:
+            print("!!!!!!!!!!")
+            print(self.data)
+            urls = filter(validators.url, self.data)
+            print("!! urls {}".format(urls))
+            for url in urls:
+                yield scrapy.Request(url=url, callback=self.parse)
+                # with open(self.result_file, 'a') as result_file:
+                #     new_column = " ".join(self.all_twitter_links)
+                #     print("!!!!!")
+                #     print(new_column)
+                #     row.append(new_column)
+                #     result_file.write(",".join(row) + "\n")
+                # self.all_twitter_links = []
+            print("! This is the end !")
+        except Exception, e:
+            print(e)
---
+++ main.py
@@ -0,0 +1,28 @@
+import csv
+import subprocess
+import sys
+import traceback
+import json
+import base64
+
+ALL_FOUND_TWITTERS = "all found twitters"
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+        with open(filename, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            header = next(reader)
+            if ALL_FOUND_TWITTERS not in header:
+                header.append(ALL_FOUND_TWITTERS)
+            result_index = header.index(ALL_FOUND_TWITTERS)
+            for row in reader:
+                cmd = ['scrapy', 'crawl', 'twitter', '-a', 'data="'+base64.b64encode(json.dumps(row))+'"']
+                print(" ".join(cmd))
+                p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+                p.wait()
+                print(p.returncode)
+    except Exception, e:
+        traceback.print_exc()
+    finally:
+        pass
