03fd48513cca9c2e54f728154d50d2c8fb85ed4b
michaelsizonenko <dethline@te.net.ua>
2019-10-25 13:40:55 +0300

    correct parsing

--- friedmansscraper/spiders/friedmans_spider.py
+++ friedmansscraper/spiders/friedmans_spider.py
@@ -2,12 +2,13 @@ import scrapy
 import json
 import validators
 import base64
-from tempfile import NamedTemporaryFile
+from urlparse import urlparse
 
 from scrapy import signals
 
 ALL_FOUND_TWITTERS = "all found twitters"
 
+popular_email_domains = ["gmail.com", "outlook.com", "yahoo.com", "icloud.com", "aol.com", "mail.com"]
 
 class FriedmansSpider(scrapy.Spider):
     name = "twitter"
@@ -36,7 +37,7 @@ class FriedmansSpider(scrapy.Spider):
         spider.logger.info('Spider closed: %s', spider.name)
         with open(self.result_file, 'a') as result_file:
             new_column = " ".join(self.all_twitter_links)
-            print(new_column)
+            self.logger.info(new_column)
             self.data.append(new_column)
             result_file.write(",".join(self.data) + "\n")
 
@@ -50,8 +51,16 @@ class FriedmansSpider(scrapy.Spider):
             self.logger.info(self.data)
             urls = filter(validators.url, self.data)
             self.logger.info("!! urls {}".format(urls))
+            email_domain = self.data[1].split("@")[1]
+            self.logger.info(email_domain)
+            if email_domain not in popular_email_domains and email_domain not in "\n".join(urls):
+                yield scrapy.Request(url=email_domain, callback=self.parse)
             for url in urls:
                 yield scrapy.Request(url=url, callback=self.parse)
+                o = urlparse(url)
+                root_url = o.scheme + "://" + o.netloc
+                if root_url != url:
+                    yield scrapy.Request(url=root_url, callback=self.parse)
             self.logger.info("! This is the end !")
         except Exception, e:
             self.logger.exception(e.message)
