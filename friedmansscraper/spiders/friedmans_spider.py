86243f20ab8906b127b6b698f0d2b7a5b537cf0a
michaelsizonenko <dethline@te.net.ua>
2019-10-27 08:32:08 +0200

    recursive search

--- friedmansscraper/spiders/friedmans_spider.py
+++ friedmansscraper/spiders/friedmans_spider.py
@@ -9,7 +9,7 @@ from scrapy import signals
 ALL_FOUND_TWITTERS = "all found twitters"
 NAME_INDEX = 1
 popular_email_domains = ["gmail.com", "outlook.com", "yahoo.com", "icloud.com", "aol.com", "mail.com"]
-exclude_twitter_links = ["https://www.twitter.com/home"]
+ignore_links = ["https://www.twitter.com/home"]
 
 
 class FriedmansSpider(scrapy.Spider):
@@ -18,14 +18,16 @@ class FriedmansSpider(scrapy.Spider):
     header = []
     result_file = "result.csv"
     current_row = ""
-    all_twitter_links = []
+    all_twitter_links = set()
     user_name = []
     email_user_name = []
     index = 10
+    requested_urls = set()
 
     custom_settings = {
         'DOWNLOAD_TIMEOUT': 10,
-        'RETRY_ENABLED': False
+        'RETRY_ENABLED': False,
+        'ROBOTSTXT_OBEY': False
     }
 
     def __init__(self, data=None, index=10, **kwargs):
@@ -53,7 +55,7 @@ class FriedmansSpider(scrapy.Spider):
         return spider
 
     def spider_closed(self, spider):
-        twitter_links = set(self.all_twitter_links)
+        twitter_links = self.all_twitter_links
         self.logger.info(twitter_links)
         spider.logger.info('Spider closed: %s', spider.name)
         while len(self.data) < int(self.index):
@@ -63,13 +65,48 @@ class FriedmansSpider(scrapy.Spider):
             self.data.append(new_column)
             result_file.write(",".join(self.data) + "\n")
 
-    def parse(self, response):
-        self.logger.info("! Parse callback !")
+    def validate_absolute_links(self, root_url, link):
+        return root_url in link \
+               and link not in self.requested_urls \
+               and ".pdf" not in link \
+               and ".jpg" not in link \
+               and ".jpeg" not in link
+
+    def validate_relative_links(self, root_url, link):
+        return len(link) > 2 \
+               and root_url + link not in self.requested_urls \
+               and link[0] == "/" and link[1] != "/" \
+               and ".pdf" not in link \
+               and ".jpg" not in link \
+               and ".jpeg" not in link
+
+    def parse(self, response, primary=False):
+        self.logger.debug("! Parse callback for URL: {} !".format(response.request.url))
+        self.logger.debug("Primary ? {}".format(primary))
         # self.all_twitter_links += filter(lambda x: "twitter" in x, response.xpath('//a/@href').extract())
-        twitter_links = filter(lambda x: "twitter.com" in x and x not in exclude_twitter_links, response.xpath('//a/@href').extract())
+        all_links = response.xpath('//a/@href').extract()
+        self.logger.debug(all_links)
+        o = urlparse(response.request.url)
+        root_url = o.scheme + "://" + o.netloc
+        if primary:
+            for link in all_links:
+                if self.validate_absolute_links(root_url, link):
+                    self.requested_urls.add(link)
+                    yield scrapy.Request(url=link, callback=self.parse, cb_kwargs={"primary": False})
+                elif self.validate_relative_links(root_url, link):
+                    link = root_url + link
+                    self.requested_urls.add(link)
+                    yield scrapy.Request(url=link, callback=self.parse, cb_kwargs={"primary": False})
+        twitter_links = filter(lambda x: "twitter.com" in x, all_links)
+        self.logger.debug(twitter_links)
         twitter_links = [x.split("?")[0] if "?" in x else x for x in twitter_links]
-        self.all_twitter_links = twitter_links
-        self.logger.info(self.all_twitter_links)
+        self.logger.debug(twitter_links)
+        twitter_links = filter(lambda x: x not in ignore_links, twitter_links)
+        for link in twitter_links:
+            if "/status/" in link:
+                link = link.split("/status/")[0]
+            self.all_twitter_links.add(link)
+        self.logger.debug(self.all_twitter_links)
 
     def start_requests(self):
         try:
@@ -79,18 +116,22 @@ class FriedmansSpider(scrapy.Spider):
             try:
                 email_domain = filter(validators.email, self.data)[0].split("@")[1]
                 if email_domain not in popular_email_domains and email_domain not in "\n".join(urls):
-                    yield scrapy.Request(url="https://" + email_domain, callback=self.parse)
+                    self.requested_urls.add("https://" + email_domain)
+                    yield scrapy.Request(url="https://" + email_domain, callback=self.parse, cb_kwargs={"primary": True})
             except Exception, e:
                 self.logger.exception(e.message)
             for url in urls:
                 try:
-                    yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+                    if url not in self.requested_urls:
+                        self.requested_urls.add(url)
+                        yield scrapy.Request(url=url, callback=self.parse, cb_kwargs={"primary": True})
                     o = urlparse(url)
                     root_url = o.scheme + "://" + o.netloc
-                    if root_url != url:
-                        yield scrapy.Request(url=root_url, callback=self.parse, dont_filter=True)
+                    if root_url != url and root_url not in self.requested_urls:
+                        self.requested_urls.add(root_url)
+                        yield scrapy.Request(url=root_url, callback=self.parse, cb_kwargs={"primary": True})
                 except Exception, e:
                     self.logger.exception(e.message)
-            self.logger.info("! This is the end !")
+            self.logger.debug("! This is the end of the row !")
         except Exception, e:
             self.logger.exception(e.message)
