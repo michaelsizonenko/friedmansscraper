f35b3cd4e703f5a7664a707703e745dd5f8dc23a
michaelsizonenko <dethline@te.net.ua>
2019-10-27 17:37:43 +0200

    name check

--- friedmansscraper/spiders/friedmans_spider.py
+++ friedmansscraper/spiders/friedmans_spider.py
@@ -8,8 +8,8 @@ from scrapy import signals
 
 ALL_FOUND_TWITTERS = "all found twitters"
 NAME_INDEX = 1
-popular_email_domains = ["gmail.com", "outlook.com", "yahoo.com", "icloud.com", "aol.com", "mail.com"]
-ignore_links = ["https://www.twitter.com/home"]
+popular_email_domains = ["gmail.com", "outlook.com", "yahoo.com", "icloud.com", "aol.com", "mail.com", "msn.com", "hotmail.com"]
+ignore_links = ['https://twitter.com/about', 'https://twitter.com/account', 'https://twitter.com/accounts', 'https://twitter.com/activity', 'https://twitter.com/all', 'https://twitter.com/announcements', 'https://twitter.com/anywhere', 'https://twitter.com/api_rules', 'https://twitter.com/api_terms', 'https://twitter.com/apirules', 'https://twitter.com/apps', 'https://twitter.com/auth', 'https://twitter.com/badges', 'https://twitter.com/blog', 'https://twitter.com/business', 'https://twitter.com/buttons', 'https://twitter.com/contacts', 'https://twitter.com/devices', 'https://twitter.com/direct_messages', 'https://twitter.com/download', 'https://twitter.com/downloads', 'https://twitter.com/edit_announcements', 'https://twitter.com/faq', 'https://twitter.com/favorites', 'https://twitter.com/find_sources', 'https://twitter.com/find_users', 'https://twitter.com/followers', 'https://twitter.com/following', 'https://twitter.com/friend_request', 'https://twitter.com/friendrequest', 'https://twitter.com/friends', 'https://twitter.com/goodies', 'https://twitter.com/help', 'https://twitter.com/home', 'https://twitter.com/im_account', 'https://twitter.com/inbox', 'https://twitter.com/invitations', 'https://twitter.com/invite', 'https://twitter.com/jobs', 'https://twitter.com/list', 'https://twitter.com/login', 'https://twitter.com/logo', 'https://twitter.com/logout', 'https://twitter.com/me', 'https://twitter.com/mentions', 'https://twitter.com/messages', 'https://twitter.com/mockview', 'https://twitter.com/newtwitter', 'https://twitter.com/notifications', 'https://twitter.com/nudge', 'https://twitter.com/oauth', 'https://twitter.com/phoenix_search', 'https://twitter.com/positions', 'https://twitter.com/privacy', 'https://twitter.com/public_timeline', 'https://twitter.com/related_tweets', 'https://twitter.com/replies', 'https://twitter.com/retweeted_of_mine', 'https://twitter.com/retweets', 'https://twitter.com/retweets_by_others', 'https://twitter.com/rules', 'https://twitter.com/saved_searches', 'https://twitter.com/search', 'https://twitter.com/sent', 'https://twitter.com/settings', 'https://twitter.com/share', 'https://twitter.com/signup', 'https://twitter.com/signin', 'https://twitter.com/similar_to', 'https://twitter.com/statistics', 'https://twitter.com/terms', 'https://twitter.com/tos', 'https://twitter.com/translate', 'https://twitter.com/trends', 'https://twitter.com/tweetbutton', 'https://twitter.com/twttr', 'https://twitter.com/update_discoverability', 'https://twitter.com/users', 'https://twitter.com/welcome', 'https://twitter.com/who_to_follow', 'https://twitter.com/widgets', 'https://twitter.com/zendesk_auth', 'https://twitter.com/media_signup']
 
 
 class FriedmansSpider(scrapy.Spider):
@@ -41,7 +41,8 @@ class FriedmansSpider(scrapy.Spider):
         self.index = index
 
     def user_name_to_list(self, user_name):
-        return user_name.split(" ")
+        user_name = user_name.lower().replace("biography", "").replace(".Jr", "").replace(".Sr", "")
+        return filter(lambda x: x != "", user_name.split(" "))
 
     def email_user_name_to_list(self, email):
         user_name = email.split("@")[0]
@@ -60,9 +61,17 @@ class FriedmansSpider(scrapy.Spider):
         spider.logger.info('Spider closed: %s', spider.name)
         while len(self.data) < int(self.index):
             self.data.append("")
+        match_twitter = [" ".join(twitter_links)]
+        for tw_item in twitter_links:
+            any_flag = any(sub_name in tw_item for sub_name in self.user_name)
+            any_flag = any_flag or any(sub_name in tw_item for sub_name in self.email_user_name)
+            all_flag = all(sub_name in tw_item for sub_name in self.user_name)
+            all_flag = all_flag or all(sub_name in tw_item for sub_name in self.email_user_name)
+            if any_flag:
+                match_twitter += [tw_item, any_flag, all_flag]
+        self.data += match_twitter
+        self.logger.debug(self.data)
         with open(self.result_file, 'a') as result_file:
-            new_column = " ".join(twitter_links)
-            self.data.append(new_column)
             result_file.write(",".join(self.data) + "\n")
 
     def validate_absolute_links(self, root_url, link):
