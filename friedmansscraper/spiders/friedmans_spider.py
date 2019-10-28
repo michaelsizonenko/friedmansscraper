033824737bc396bb77f103a19b5edd8693ab8aab
michaelsizonenko <dethline@te.net.ua>
2019-10-28 17:17:27 +0200

    add config file

--- friedmansscraper/spiders/friedmans_spider.py
+++ friedmansscraper/spiders/friedmans_spider.py
@@ -7,7 +7,6 @@ from urlparse import urlparse
 from scrapy import signals
 
 ALL_FOUND_TWITTERS = "all found twitters"
-NAME_INDEX = 1
 popular_email_domains = ["gmail.com", "outlook.com", "yahoo.com", "icloud.com", "aol.com", "mail.com", "msn.com", "hotmail.com"]
 ignore_links = ['https://twitter.com/about', 'https://twitter.com/account', 'https://twitter.com/accounts', 'https://twitter.com/activity', 'https://twitter.com/all', 'https://twitter.com/announcements', 'https://twitter.com/anywhere', 'https://twitter.com/api_rules', 'https://twitter.com/api_terms', 'https://twitter.com/apirules', 'https://twitter.com/apps', 'https://twitter.com/auth', 'https://twitter.com/badges', 'https://twitter.com/blog', 'https://twitter.com/business', 'https://twitter.com/buttons', 'https://twitter.com/contacts', 'https://twitter.com/devices', 'https://twitter.com/direct_messages', 'https://twitter.com/download', 'https://twitter.com/downloads', 'https://twitter.com/edit_announcements', 'https://twitter.com/faq', 'https://twitter.com/favorites', 'https://twitter.com/find_sources', 'https://twitter.com/find_users', 'https://twitter.com/followers', 'https://twitter.com/following', 'https://twitter.com/friend_request', 'https://twitter.com/friendrequest', 'https://twitter.com/friends', 'https://twitter.com/goodies', 'https://twitter.com/help', 'https://twitter.com/home', 'https://twitter.com/im_account', 'https://twitter.com/inbox', 'https://twitter.com/invitations', 'https://twitter.com/invite', 'https://twitter.com/jobs', 'https://twitter.com/list', 'https://twitter.com/login', 'https://twitter.com/logo', 'https://twitter.com/logout', 'https://twitter.com/me', 'https://twitter.com/mentions', 'https://twitter.com/messages', 'https://twitter.com/mockview', 'https://twitter.com/newtwitter', 'https://twitter.com/notifications', 'https://twitter.com/nudge', 'https://twitter.com/oauth', 'https://twitter.com/phoenix_search', 'https://twitter.com/positions', 'https://twitter.com/privacy', 'https://twitter.com/public_timeline', 'https://twitter.com/related_tweets', 'https://twitter.com/replies', 'https://twitter.com/retweeted_of_mine', 'https://twitter.com/retweets', 'https://twitter.com/retweets_by_others', 'https://twitter.com/rules', 'https://twitter.com/saved_searches', 'https://twitter.com/search', 'https://twitter.com/sent', 'https://twitter.com/settings', 'https://twitter.com/share', 'https://twitter.com/signup', 'https://twitter.com/signin', 'https://twitter.com/similar_to', 'https://twitter.com/statistics', 'https://twitter.com/terms', 'https://twitter.com/tos', 'https://twitter.com/translate', 'https://twitter.com/trends', 'https://twitter.com/tweetbutton', 'https://twitter.com/twttr', 'https://twitter.com/update_discoverability', 'https://twitter.com/users', 'https://twitter.com/welcome', 'https://twitter.com/who_to_follow', 'https://twitter.com/widgets', 'https://twitter.com/zendesk_auth', 'https://twitter.com/media_signup']
 
@@ -23,6 +22,8 @@ class FriedmansSpider(scrapy.Spider):
     email_user_name = []
     index = 10
     requested_urls = set()
+    depth = 5
+    name_index = 1
 
     custom_settings = {
         'DOWNLOAD_TIMEOUT': 10,
@@ -30,15 +31,17 @@ class FriedmansSpider(scrapy.Spider):
         'ROBOTSTXT_OBEY': False
     }
 
-    def __init__(self, data=None, index=10, **kwargs):
+    def __init__(self, data=None, index=10, depht=5, name_index=1, **kwargs):
         super(FriedmansSpider, self).__init__(**kwargs)
         if not data:
             raise ValueError("%s must have a data" % type(self).__name__)
         self.data = json.loads(base64.b64decode(data))
         self.logger.info(self.data)
-        self.user_name = self.user_name_to_list(self.data[NAME_INDEX])
+        self.name_index = int(name_index)
+        self.user_name = self.user_name_to_list(self.data[self.name_index])
         self.email_user_name = self.email_user_name_to_list(filter(validators.email, self.data)[0])
-        self.index = index
+        self.index = int(index)
+        self.depth = int(depht)
 
     def user_name_to_list(self, user_name):
         user_name = user_name.lower().replace("biography", "").replace(".Jr", "").replace(".Sr", "")
@@ -59,9 +62,9 @@ class FriedmansSpider(scrapy.Spider):
         twitter_links = self.all_twitter_links
         self.logger.info(twitter_links)
         spider.logger.info('Spider closed: %s', spider.name)
-        while len(self.data) < int(self.index):
+        while len(self.data) < self.index:
             self.data.append("")
-        match_twitter = [" ".join(twitter_links)]
+        match_twitter = ["; ".join(twitter_links)]
         for tw_item in twitter_links:
             any_flag = any(sub_name in tw_item for sub_name in self.user_name)
             any_flag = any_flag or any(sub_name in tw_item for sub_name in self.email_user_name)
@@ -89,23 +92,23 @@ class FriedmansSpider(scrapy.Spider):
                and ".jpg" not in link \
                and ".jpeg" not in link
 
-    def parse(self, response, primary=False):
+    def parse(self, response, depth=1):
         self.logger.debug("! Parse callback for URL: {} !".format(response.request.url))
-        self.logger.debug("Primary ? {}".format(primary))
+        self.logger.debug("depth : {}".format(depth))
         # self.all_twitter_links += filter(lambda x: "twitter" in x, response.xpath('//a/@href').extract())
         all_links = response.xpath('//a/@href').extract()
         self.logger.debug(all_links)
         o = urlparse(response.request.url)
         root_url = o.scheme + "://" + o.netloc
-        if primary:
+        if depth < self.depth:
             for link in all_links:
                 if self.validate_absolute_links(root_url, link):
                     self.requested_urls.add(link)
-                    yield scrapy.Request(url=link, callback=self.parse, cb_kwargs={"primary": False})
+                    yield scrapy.Request(url=link, callback=self.parse, cb_kwargs={"depth": depth+1})
                 elif self.validate_relative_links(root_url, link):
                     link = root_url + link
                     self.requested_urls.add(link)
-                    yield scrapy.Request(url=link, callback=self.parse, cb_kwargs={"primary": False})
+                    yield scrapy.Request(url=link, callback=self.parse, cb_kwargs={"depth": depth+1})
         twitter_links = filter(lambda x: "twitter.com" in x, all_links)
         self.logger.debug(twitter_links)
         twitter_links = [x.split("?")[0] if "?" in x else x for x in twitter_links]
@@ -114,6 +117,9 @@ class FriedmansSpider(scrapy.Spider):
         for link in twitter_links:
             if "/status/" in link:
                 link = link.split("/status/")[0]
+            link = link.replace("mobile.", "")
+            link = link.replace("www.", "")
+            link = link.replace("http://", "https://")
             self.all_twitter_links.add(link)
         self.logger.debug(self.all_twitter_links)
 
@@ -126,19 +132,19 @@ class FriedmansSpider(scrapy.Spider):
                 email_domain = filter(validators.email, self.data)[0].split("@")[1]
                 if email_domain not in popular_email_domains and email_domain not in "\n".join(urls):
                     self.requested_urls.add("https://" + email_domain)
-                    yield scrapy.Request(url="https://" + email_domain, callback=self.parse, cb_kwargs={"primary": True})
+                    yield scrapy.Request(url="https://" + email_domain, callback=self.parse, cb_kwargs={"depth": 1})
             except Exception, e:
                 self.logger.exception(e.message)
             for url in urls:
                 try:
                     if url not in self.requested_urls:
                         self.requested_urls.add(url)
-                        yield scrapy.Request(url=url, callback=self.parse, cb_kwargs={"primary": True})
+                        yield scrapy.Request(url=url, callback=self.parse, cb_kwargs={"depth": 1})
                     o = urlparse(url)
                     root_url = o.scheme + "://" + o.netloc
                     if root_url != url and root_url not in self.requested_urls:
                         self.requested_urls.add(root_url)
-                        yield scrapy.Request(url=root_url, callback=self.parse, cb_kwargs={"primary": True})
+                        yield scrapy.Request(url=root_url, callback=self.parse, cb_kwargs={"depth": 1})
                 except Exception, e:
                     self.logger.exception(e.message)
             self.logger.debug("! This is the end of the row !")
--- main.py
+++ main.py
@@ -4,29 +4,39 @@ import sys
 import traceback
 import json
 import base64
+import os
 
 ALL_FOUND_TWITTERS = "all found twitters"
 MATCH_TWITTER = "match twitter "
 ONE_W = "1 WORD MATCH"
 ALL_W = "ALL NAME WORDS MATCH"
 
+CONFIG_FILE = "config.json"
+CONFIG_PARAMS = {"depth", "name_index", "input_file", "start_from", "continue_processing", "process_until"}
+
 if __name__ == "__main__":
     try:
-        filename = sys.argv[1]
-        start_from = 0
-        end_with = 100000
-        continue_parsing = False
+        assert os.path.exists(CONFIG_FILE), "This is weird! Config file does not exists"
+        with open(CONFIG_FILE) as config_file:
+            config = json.load(config_file)
+        assert set([x.encode('utf-8') for x in config.keys()]).issubset(CONFIG_PARAMS), "This is weird! Config file contains unexpected params"
+        assert set([x.encode('utf-8') for x in config.keys()]).issuperset(CONFIG_PARAMS), "This is weird! Config file contains not enough params"
+        assert os.path.exists(config["input_file"]), "This is weird! The input file does not exists"
+        filename = config["input_file"]
+        start_from = config["start_from"]
+        process_until = config["process_until"]
+        continue_processing = config["continue_processing"]
         if len(sys.argv) > 2:
             start_from = int(sys.argv[2])
         print("start from {}".format(start_from))
         if len(sys.argv) > 3:
-            end_with = int(sys.argv[3])
-        print("end with {}".format(end_with))
-        assert start_from < end_with, "Are you serious ?"
+            process_until = int(sys.argv[3])
+        print("end with {}".format(process_until))
+        assert start_from < process_until, "Are you serious ?"
         if len(sys.argv) > 4:
             print(sys.argv[4])
-            continue_parsing = bool(sys.argv[4])
-            print(continue_parsing)
+            continue_processing = bool(sys.argv[4])
+            print(continue_processing)
         with open(filename, 'r') as csv_file:
             reader = csv.reader(csv_file)
             header = next(reader)
@@ -40,7 +50,7 @@ if __name__ == "__main__":
                     header.append(MATCH_TWITTER + str(i+1))
                     header.append(ONE_W)
                     header.append(ALL_W)
-            if not continue_parsing:
+            if not continue_processing:
                 with open("result.csv", 'w') as result_file:
                     result_file.write(",".join(header) + "\n")
             counter = 0
@@ -50,10 +60,15 @@ if __name__ == "__main__":
                 print(start_from)
                 if start_from > counter:
                     continue
-                if counter > end_with:
+                if counter > process_until:
                     continue
-                cmd = ['scrapy', 'crawl', 'twitter', '-a', 'index={}'.format(index), '-a',
-                       'data="'+base64.b64encode(json.dumps(row))+'"']
+                cmd = [
+                    'scrapy', 'crawl', 'twitter',
+                    '-a', 'index={}'.format(index),
+                    '-a', 'data="'+base64.b64encode(json.dumps(row))+'"',
+                    '-a', 'depth='+str(config["depth"]),
+                    '-a', 'name_index='+str(config["name_index"])
+                       ]
                 print(" ".join(cmd))
                 p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
                 p.wait()
